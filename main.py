1c77ddb05c12d40c42db13296c3c5bcecd6c4f63
CyberSurge-Dev <82483553+CyberSurge-Dev@users.noreply.github.com>
2023-09-19 14:17:57 +0000

    Updated main (unstable)

--- main.py
+++ main.py
@@ -26,9 +26,21 @@ class Game():
         pygame.init()
 
         self.settings = Settings() # Initiate settings class
-        self.screen = pygame.display.set_mode(self.settings.screen_size) # Set screen size
-        self.keybinds = self.settings.keybinds # gets a dictionary for game keybinds
 
+        # Create seperate display and screen elements to be able to easily scale to any screen size
+        self.display = pygame.Surface((640, 360)) # Only write to this surface 
+        self.screen = pygame.display.set_mode(self.settings.screen_size) # Set screen size
+        
+        self.keybinds = self.settings.keybinds # gets a dictionary for game keybinds
+        self.sWidth = pygame.display.get_window_size()[0]
+        self.sHeight = pygame.display.get_window_size()[1]
+
+        # Determine the smallest 16:9 ratio that can fit in the screen for the display size
+        self.dWidth = self.sWidth - (self.sWidth % 16)
+        self.dHeight = self.sHeight - (self.sHeight % 9)
+
+
+        
         self.clock = pygame.time.Clock() # Create the game clock
 
 
@@ -60,7 +72,14 @@ class Game():
                         pass
                     if event.key == self.keybinds["right"]: # Check for the right button being released
                         pass
-                        
+
+            self.screen.fill((20, 20, 20))
+            self.display.fill((30, 30, 30))
+            
+
+            self.screen.blit(pygame.transform.scale(self.display, (self.dWidth, self.dHeight)), 
+                             ((self.sWidth/2)-(self.display.get_size()[0]/2), (self.sHeight/2)-(self.display.get_size()[1]/2))))
+            
             pygame.display.update() # Refresh the display
             self.clock.tick(60) # Limit FPS to 60 
 
