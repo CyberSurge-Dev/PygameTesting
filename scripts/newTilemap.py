9b70ad1bbe24c4d0e91d6a9485e9f9240c1fffd4
cybersurge-dev <zhoover.nc@gmail.com>
2023-10-30 14:14:59 -0400

    Started adding the implimentation of the new tilemap

---
+++ scripts/newTilemap.py
@@ -0,0 +1,14 @@
+# Created By: Zachary Hoover
+# Created Date: 10/30/2023
+# Version: 2.0
+# --------------------------------------------------------------------------------
+"""
+This file contains the re-written tilemap, which was re-written to support
+more customizable tile objects, and contains more robust, and simpiler to work 
+with methods.
+
+--+ Classes +-- 
+Tile() - Base tile
+"""
+# --------------------------------------------------------------------------------
+
---
+++ scripts/tiles.py
@@ -0,0 +1,16 @@
+# Created By: Zachary Hoover
+# Created Date: 10/30/2023
+# Version: 1.0
+# --------------------------------------------------------------------------------
+"""
+This file contains the Tile object classes, used for creating tiles simply.
+
+--+ Classes +-- 
+Tile() - Base tile
+"""
+# --------------------------------------------------------------------------------
+
+class Tile():
+    """Base class for gam tiles"""
+    def __init__():
+        pass
